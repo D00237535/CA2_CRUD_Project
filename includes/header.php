81970f65032b78a8067862ce58e615364ede9f35
D00237535 <D00237535@student.dkit.ie>
2022-02-25 09:15:22 +0000

    scss

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <!-- the head section -->
 <head>
-<title>My PHP CRUD App</title>
-<link rel="stylesheet" type="text/css" href="main.css">
+<title>My Modes For League of Legends</title>
+<link rel="stylesheet" type="text/css" href="CSS/main.css">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My PHP CRUD app</h1></header>
+<header><h1>Many Modes of League of Legends</h1></header>
